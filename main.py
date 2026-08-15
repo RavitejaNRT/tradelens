e54afc4f55428414183f83a32a7ac7745e22f6d2
Raviteja <teja.classy999@gmail.com>
2026-08-15 18:22:21 +0530

    Separate trade data from application logic

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
 from trade_calculator import calculate_trade
-
-trades = [
-    {
-        "symbol": "RELIANCE",
-        "entry": 500,
-        "stop_loss": 480,
-        "target": 540
-    },
-    {
-        "symbol": "TCS",
-        "entry": 1000,
-        "stop_loss": 960,
-        "target": 1050
-    },
-    {
-        "symbol": "INFY",
-        "entry": 1500,
-        "stop_loss": 1450,
-        "target": 1600
-    }
-]
-
+from trade_data import trades
 
 for trade_input in trades:
     trade = calculate_trade(
---
+++ trade_data.py
@@ -0,0 +1,20 @@
+trades = [
+    {
+        "symbol": "RELIANCE",
+        "entry": 500,
+        "stop_loss": 480,
+        "target": 540
+    },
+    {
+        "symbol": "TCS",
+        "entry": 1000,
+        "stop_loss": 960,
+        "target": 1050
+    },
+    {
+        "symbol": "INFY",
+        "entry": 1500,
+        "stop_loss": 1450,
+        "target": 1600
+    }
+]
